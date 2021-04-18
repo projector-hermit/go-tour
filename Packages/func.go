f86ead62d0bce22a0be55d705523a5bd92b21c95
projector-hermit <hermit_github@mail.ru>
2021-04-18 16:02:38 +0300

    почистил код

--- Packages/func.go
+++ Packages/func.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func add(x, y int) int {
 	return x + y
@@ -9,8 +11,24 @@ func add(x, y int) int {
 func swap(x, y string) (string, string) {
 	return y, x
 }
+func split(sum int) (x, y int) {
+	x = sum * 1 / 2
+	y = sum - x
+	return
+}
+
 func main() {
+	var (
+		peace, door, ball bool
+		t, j              int = 1, 2
+		i                 int
+		c, python, java   = true, false, "no!"
+		a, b              = swap("вселенная", "бесконечна")
+	)
+	fmt.Println(i, peace, door, ball)
+	fmt.Println(t, j, c, python, java)
 	fmt.Println(add(60, 9))
-	a, b := swap("вселенная", "бесконечна")
 	fmt.Println(a, b)
+	fmt.Println(split(176))
+
 }
