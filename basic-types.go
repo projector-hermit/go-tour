003a89bbc2cdd93409e2cd2e31fb773ff608be3a
projector-hermit <hermit_github@mail.ru>
2021-04-20 20:46:51 +0300

    upd

--- basic-types.go
+++ basic-types.go
@@ -9,12 +9,12 @@ var (
 	ToBe   bool       = true
 	MaxInt uint64     = 20000 / 2 * 75
 	z      complex128 = cmplx.Sqrt(69)
-	w      int
-	t      bool
-	f      string
 )
 
 func main() {
+	w := 7
+	t := false
+	f := " "
 	fmt.Printf("Тип: %T Значение: %v\n", ToBe, ToBe)
 	fmt.Printf("Тип: %T Значение: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Тип: %T Значение: %v\n", z, z)
