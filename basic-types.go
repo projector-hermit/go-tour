29ac10c1dac0415fb1147bb7b5a0a47eda13d706
projector-hermit <hermit_github@mail.ru>
2021-04-20 20:11:24 +0300

    upd

---
+++ basic-types.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx" // предоставляет основные константы и математические функции для комплексных чисел.
+)
+
+var (
+	ToBe   bool       = true
+	MaxInt uint64     = 20000 / 2 * 75
+	z      complex128 = cmplx.Sqrt(69)
+)
+
+func main() {
+	fmt.Printf("Тип: %T Значение: %v\n", ToBe, ToBe)
+	fmt.Printf("Тип: %T Значение: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Тип: %T Значение: %v\n", z, z)
+}
