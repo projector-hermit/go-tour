8e0c4f760e3ffa76f165292d9c65b2e50086bc7b
projector-hermit <hermit_github@mail.ru>
2021-04-23 07:49:54 +0300

    morning

--- solid2.go
+++ solid2.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var x [6]float64
-	x[0] = 99
-	x[1] = 98
-	x[2] = 77
-	x[3] = 97
-	x[4] = 87
-	x[5] = 5
+	x := [6]float64{
+		99,
+		55,
+		75,
+		98,
+		25,
+	}
 
 	var total float64 = 0
 	for _, value := range x {
