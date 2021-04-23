881e3def0322879ca8a067a18b0527d27e205845
projector-hermit <hermit_github@mail.ru>
2021-04-23 07:35:26 +0300

    morning

--- solid2.go
+++ solid2.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var x [5]float64
-	x[0] = 92
-	x[1] = 26
+	var x [6]float64
+	x[0] = 99
+	x[1] = 98
 	x[2] = 77
-	x[3] = 54
+	x[3] = 97
 	x[4] = 87
+	x[5] = 5
 
 	var total float64 = 0
-	for i := 0; i < 5; i++ {
+	for i := 0; i < len(x); i++ {
 		total += x[i]
 	}
-	fmt.Println(total / 5)
+	fmt.Println(total / float64(len(x)))
 }
